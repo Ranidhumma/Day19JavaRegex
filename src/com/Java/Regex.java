05d0124ab941538293f8ffc1be5d0cbba65405d1
Ranidhumma <ranidhumma8@gmail.com>
2022-03-11 10:05:22 +0530

    First Name start with caps

--- src/com/Java/Regex.java
+++ src/com/Java/Regex.java
@@ -1,10 +1,22 @@
 package com.Java;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("Welcome to user registartion");
 
+		Pattern p = Pattern.compile("[A-Z]{1}[a-z]{2,}");
+		Matcher m = p.matcher("Axy");
+		boolean b = m.matches();
+		// System.out.println(b);
+		if (b)
+			System.out.println("Valid Input passed");
+		else
+			System.out.println("Invalid input passed");
 	}
+
 }
